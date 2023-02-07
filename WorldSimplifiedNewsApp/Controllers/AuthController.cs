f241d0ddbc3e43a4a85970b5c8e8bac86c173d80
Sergiu <sergiu.buru99@gmail.com>
2023-02-06 20:36:23 -0600

    added template for auth controller

---
+++ WorldSimplifiedNewsApp/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldSimplifiedNewsApp.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Resgister()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("NewAccessToken")]
+        public async Task<IActionResult> NewAccessToken()
+        {
+            return Ok();
+        }
+    }
+}
