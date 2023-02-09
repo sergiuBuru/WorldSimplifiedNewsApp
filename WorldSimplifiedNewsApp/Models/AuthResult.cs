b53b17c6acdbfedcf713f27203edb3a281a02e0e
Sergiu <sergiu.buru99@gmail.com>
2023-02-08 20:46:18 -0600

    added data model for an auth response

---
+++ WorldSimplifiedNewsApp/Models/AuthResult.cs
@@ -0,0 +1,9 @@
+﻿namespace WorldSimplifiedNewsApp.Models
+{
+    public class AuthResult
+    {
+        public string Token { get; set; }
+        public bool Success { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
