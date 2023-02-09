a1f70e75b5c0f5643728884be2da99cc02d04990
Sergiu <sergiu.buru99@gmail.com>
2023-02-08 20:51:33 -0600

    added routes for registration and login and for serving a jwt

--- WorldSimplifiedNewsApp/Controllers/AuthController.cs
+++ WorldSimplifiedNewsApp/Controllers/AuthController.cs
@@ -1,22 +1,127 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using WorldSimplifiedNewsApp.Models;
+using WorldSimplifiedNewsApp.Models.DTOs;
 
 namespace WorldSimplifiedNewsApp.Controllers
 {
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
         [HttpPost]
         [Route("Register")]
-        public async Task<IActionResult> Resgister()
+        public async Task<IActionResult> Register([FromBody] UserRegisterRequestDto registerRequest)
         {
-            return Ok(DotNetEnv.Env.GetString("TEST_STRING"));
+            if (ModelState.IsValid)
+            {
+                // check if the email is already used
+                var user_exists = await _userManager.FindByEmailAsync(registerRequest.Email);
+
+                if(user_exists != null)
+                {
+                    return BadRequest(new AuthResult()
+                    {
+                        Success = false,
+                        Errors = new List<string>() { "email already exists" }
+                    });
+                }
+
+                // create the new user
+                var new_user = new IdentityUser()
+                {
+                    Email = registerRequest.Email,
+                    UserName = registerRequest.UserName,
+                };
+
+                var is_created = await _userManager.CreateAsync(new_user, registerRequest.Password);
+
+                if(is_created.Succeeded)
+                {
+                    // generate token
+                    var jwt = await GenerateToken(new_user);
+
+                    return Ok(jwt);
+                }
+
+                return BadRequest(new AuthResult()
+                {
+                    Errors = new List<string>()
+                    {
+                        "server error"
+                    }
+                });
+            }
+            return BadRequest(new AuthResult()
+            {
+                Errors = new List<string>()
+                    {
+                       "invalid payload"
+                    }
+            });
         }
 
         [HttpPost]
         [Route("Login")]
-        public async Task<IActionResult> Login()
+        public async Task<IActionResult> Login([FromBody] UserLoginRequestDto loginRequest)
         {
-            return Ok();
+            // validate the incoming request
+            if (ModelState.IsValid)
+            {
+                // check if the email exists
+                var existing_user = await _userManager.FindByEmailAsync(loginRequest.Email);
+
+                if (existing_user == null)
+                {
+                    return BadRequest(new AuthResult()
+                    {
+                        Errors = new List<String>()
+                        {
+                            "Invalid Payload"
+                        },
+                        Success = false
+                    });
+                }
+                // check if the email and password match
+                var correctPassword = await _userManager.CheckPasswordAsync(existing_user, loginRequest.Password);
+                
+                if(correctPassword == false)
+                {
+                    return BadRequest(new AuthResult()
+                    {
+                        Errors = new List<String>()
+                        {
+                            "Invalid credentials"
+                        },
+                        Success = false
+                    });
+                }
+
+                // generate the access token
+                var jwt = await GenerateToken(existing_user);
+                return Ok(jwt);
+
+            }
+            return BadRequest(new AuthResult()
+            {
+                Errors = new List<String>()
+                {
+                    "Invalid payload"
+                },
+                Success = false
+            });
         }
 
         [HttpPost]
@@ -24,6 +129,38 @@ namespace WorldSimplifiedNewsApp.Controllers
         public async Task<IActionResult> NewAccessToken()
         {
             return Ok();
+        }
+
+        private async Task<AuthResult> GenerateToken(IdentityUser user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.UTF8.GetBytes(_configuration.GetSection("JwtConfig:Secret").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Id", user.Id),
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToUniversalTime().ToString())
+                }),
+                Issuer = "https://localhost:7218",
+                Audience = "https://localhost:7218",
+                Expires = DateTime.UtcNow.Add(TimeSpan.Parse(_configuration.GetSection("JwtConfig:ExpiryTimeFrame").Value)),
+                NotBefore = DateTime.Now,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = jwtTokenHandler.WriteToken(token);
+
+            return new AuthResult()
+            {
+                Token = jwtToken,
+                Success = true
+            };
         }
     }
 }
